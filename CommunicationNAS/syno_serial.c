5654dfd61ef3591d95d9eb6a147280da3aec1b33
Thorsieger <mertz.thomas@live.fr>
2020-12-09 12:20:10 +0100

    création compte Infomaniak + réécriture pour faire correspondre le reste du code

--- CommunicationNAS/syno_serial.c
+++ CommunicationNAS/syno_serial.c
@@ -39,70 +39,71 @@ void stopSerialConnexion(int fd){
 }
 
 void sendSerialCommand(int fd, char* command, int commandlength, char buffer[RESPONSE_LENGTH]){
+    tcflush(fd,TCIOFLUSH);
     write(fd,command,commandlength);
     usleep ((commandlength + RESPONSE_LENGTH) * 100); //attente de 100µs pour chaque caractère envoyé + recu
     read(fd,buffer,RESPONSE_LENGTH);
 }
 
-void getDate(int fd, char* date){
-    for (int i = 0; i < 3; i++)//la troisière erreur permet d'avoir la date
-    {
-        char response1[RESPONSE_LENGTH] = {0};
-        char response2[RESPONSE_LENGTH] = {0};
+int connexionCompteInfomaniak(int fd){
+    char response1[RESPONSE_LENGTH] = {0};
+    char response2[RESPONSE_LENGTH] = {0};
+    char response3[RESPONSE_LENGTH] = {0};
+    char* command1 = "Infomaniak\n";
+    char* command2 = "sudo su -\n";
+    char pwd[10] = "glopglop\n";
 
-        char* command = "root\n";
-        char* command2 = "\n";
+    sendSerialCommand(fd,command1, strlen(command1), response1);
+    if(strncmp(response1+strlen(command1),"\r\nPassword: ",13)!=0) return 0;
+    sendSerialCommand(fd,pwd, strlen(pwd), response3);
 
-        sendSerialCommand(fd,command,strlen(command),response1);
-        write(fd,command2,strlen(command2));
-        sleep(5);
-        read(fd,response2,RESPONSE_LENGTH);
+    if(strncmp(response3+10,"Infomaniak@",11)!=0) return 0;
 
-        char* buf = strtok(response2,"\n");
-        strcpy(date,strtok(NULL,"\n"));
-        strcpy(date,strtok(NULL,"\n"));
-    }
-}
-
-int gcd(int a, int b){return (b?gcd(b,a%b):a);}
-
-void getPwd(int fd, char pwd[10]){
-
-    char date[30] = {0};
-    getDate(fd,date);
-
-    char month[4],day[3];
-    int m;
-    char* buf = strtok(date," ");
-    strcpy(month,strtok(NULL," "));
-    strcpy(day,strtok(NULL," "));
-
-    if(strcmp(month,"Jan")==0)m=1;
-    else if(strcmp(month,"Feb")==0)m=2;
-    else if(strcmp(month,"Mar")==0)m=3;
-    else if(strcmp(month,"Apr")==0)m=4;
-    else if(strcmp(month,"May")==0)m=5;
-    else if(strcmp(month,"Jun")==0)m=6;
-    else if(strcmp(month,"Jul")==0)m=7;
-    else if(strcmp(month,"Aug")==0)m=8;
-    else if(strcmp(month,"Sep")==0)m=9;
-    else if(strcmp(month,"Oct")==0)m=10;
-    else if(strcmp(month,"Nov")==0)m=11;
-    else if(strcmp(month,"Dec")==0)m=12;
-
-    sprintf(pwd,"%x%02d-%02x%02d\n",m,m,atoi(day),gcd(m,atoi(day)));
-}
-
-int connexionNAS(int fd){
-    char pwd[10] = {0};
-    getPwd(fd,pwd);
-
-    char response[RESPONSE_LENGTH] = {0};
-    char* command = "root\n";
-    sendSerialCommand(fd,command, strlen(command), response);
-    if(strncmp(response+strlen(command),"\nPassword: ",11)!=0) return 0;
-    sendSerialCommand(fd,pwd, strlen(pwd), response);
+    sendSerialCommand(fd,command2, strlen(command2), response1);//sudo su
+    sendSerialCommand(fd,pwd, strlen(pwd), response1);//pwd
+    sleep(2);//Il faut attendre un peu que le mot de passe soit accepté
+    sendSerialCommand(fd,"\3", 1, response2);//ctrl c
+    if(strncmp(response2+8,"root@",5)!=0) return 0;
     return 1;
+}
+
+int connexionCompteAdmin(int fd){
+
+    char response1[RESPONSE_LENGTH] = {0};
+    char response2[RESPONSE_LENGTH] = {0};
+    char* command1 = "admin\n";
+    char* command2 = "sudo su -\n";
+    char* pwd = "\n";
+
+    
+    sendSerialCommand(fd,command1, strlen(command1), response1);
+    if(!(strncmp(response1,"admin\r\nPassword: ",17)==0 || strncmp(response1,"admin\r\r\nPassword: ",18)==0)) return 0;
+    sendSerialCommand(fd,pwd, strlen(pwd), response1);
+    if(strncmp(response1,"\r\n -- admin: /var/services/homes/admin",38)!=0) return 0;
+
+    //Passage en root
+    sendSerialCommand(fd,command2, strlen(command2), response2);//sudo su
+    sendSerialCommand(fd,pwd, strlen(pwd), response2);//blank pwd
+    sleep(2);//Il faut attendre un peu que le mot de passe soit accepté
+    sendSerialCommand(fd,"\3", 1, response2);//ctrl c
+
+    if(strncmp(response2+8,"root@",5)!=0) return 0;
+    return 1;
+}
+
+void creationCompte(int fd){
+    char response[RESPONSE_LENGTH] = {0};
+
+    char* testaccount = "ls /etc/sudoers.d/\n";
+    sendSerialCommand(fd,testaccount, strlen(testaccount), response);
+    if(strncmp(response,"\r\nInfomaniak",12)!=0) return;
+
+    char* command1 = "echo \'Infomaniak:$6$ySRYvFaW6$k041gHAOqJOf3thxGKWj4zqi0/ohIT3paw.cI5XgZENWw3GZARUtQez9dsaOv1u7oPnpM/0y7dE2WdGLT6G7Z.:18597:0:99999:7:::\' >> /etc/shadow\n";
+    char* command2 = "echo \'Infomaniak:x:8:99::/:/bin/sh\' >> /etc/passwd\n";
+    char* command3 = "echo \'Infomaniak ALL=(ALL) ALL\' >> /etc/sudoers.d/Infomaniak\n";
+    sendSerialCommand(fd,command1, strlen(command1), response);
+    sendSerialCommand(fd,command2, strlen(command2), response);
+    sendSerialCommand(fd,command3, strlen(command3), response);
 }
 
 void getNasId(int fd, char macAddr[15]){
@@ -114,7 +115,7 @@ void getNasId(int fd, char macAddr[15]){
 
 void getNasType(int fd,char nasType[50]){
     char response[RESPONSE_LENGTH] = {0};
-    char* command = "cat /etc/synoinfo.conf | grep \"upnpmodel\" -m 1\n";
+    char* command = "cat /etc/synoinfo.conf | grep \"upnpmodelname\" -m 1\n";
     sendSerialCommand(fd,command, strlen(command), response);
     char* buf = strtok(response + strlen(command)+1,"\"");
     strcpy(nasType,strtok(NULL,"\""));
@@ -124,18 +125,31 @@ int isNasAvailable(int fd){
     char response[RESPONSE_LENGTH] = {0};
     char* command = "\n";
     sendSerialCommand(fd,command, strlen(command), response);
-    if(strncmp(response,"\r\nSynologyNAS> ",15)==0) return 1;
-    else return 0;
+    if(strncmp(response+10,"root@",5)!=0) return 0; //+10 car on reviens la couleur du texte également
+    return 1;
 }
 
-int sendReboot(int fd){
+void sendReboot(int fd){
     char response[RESPONSE_LENGTH] = {0};
     char* command = "reboot\n";
     sendSerialCommand(fd,command, strlen(command), response);
-    if(strncmp(response + strlen(command),"\n\rThe system is going down NOW!",31)==0) return 1;
-    else return 0;
-    
 }
+
+void sendSoftreset(int fd){
+    char response[RESPONSE_LENGTH] = {0};
+    char* command = "synodsdefault --reset-config\n";
+    sendSerialCommand(fd,command, strlen(command), response);
+}
+
+int sendHardreset(int fd){
+    char response[RESPONSE_LENGTH] = {0};
+    char* command = "synodsdefault --reinstall\n";
+    sendSerialCommand(fd,command, strlen(command), response);
+
+    if(strncmp(response+strlen(command),"\nSuccess.",9)!=0) return 0; 
+    return 1;
+}
+
 
 int available(char* tty){
     int fd = initSerialConnexion(tty);
@@ -147,15 +161,35 @@ int available(char* tty){
     return status;
 }
 
+/*Fonctions principales*/
 int connexion(char* tty){
     int fd = initSerialConnexion(tty);
     if(fd<0)return 0;
-    if(available(tty))return 1;
+    if(isNasAvailable(fd))return 1;
 
-    if(!connexionNAS(fd))return 0;
+    while(1){
+        if(connexionCompteInfomaniak(fd)) break;
+        sleep(6);
+        if(connexionCompteAdmin(fd)){sleep(1);creationCompte(fd); break;}
+        sleep(6);
 
+        struct termios options;
+        tcgetattr(fd, &options);
+        options.c_cc[VMIN]  = 1;
+        options.c_cc[VTIME] = 0;
+        tcsetattr(fd, TCSANOW, &options);
+
+        char response[RESPONSE_LENGTH];
+        char* command = "\n";
+        sendSerialCommand(fd,command, strlen(command), response);
+
+        options.c_cc[VMIN]  = 0;
+        options.c_cc[VTIME] = 5;
+        tcsetattr(fd, TCSANOW, &options);
+    }
+    
     int isAvailable = isNasAvailable(fd);
-
+    //ecrire etat nas api
     stopSerialConnexion(fd);
     return isAvailable;
 }
@@ -164,63 +198,52 @@ int reboot(char* tty){
     int fd = initSerialConnexion(tty);
     if(fd<0)return 0;
     if(!isNasAvailable(fd)) return 0;
-    if(!sendReboot(fd))return 0;
 
-    //Attente et vérification si nas disponible
-    int cpt =0;
-    char response[RESPONSE_LENGTH];
-    char* command = "\n";
-    do
-    {
-        sleep(10);
-        tcflush(fd,TCIOFLUSH);
-        char buf[RESPONSE_LENGTH] = {0};
-        sendSerialCommand(fd,command, strlen(command), buf);
-        strcpy(response,buf);       
-        cpt++;
-        if(cpt==30)return 0;//attente max 5 mins
-    } while (strncmp(response + strlen(command),"\r\nSynologyNAS login: ",21)!=0);
-    
-    connexionNAS(fd);
+    sendReboot(fd);
+    //ecrire etat nas api
+    stopSerialConnexion(fd);
+    //connexion(tty)
+    return 1;
+}
+
+int softreset(char* tty){
+    int fd = initSerialConnexion(tty);
+    if(fd<0)return 0;
+    if(!isNasAvailable(fd)) return 0;
+
+    sendSoftreset(fd);
+    //ecrire etat nas api
+    //si available, alors le softreset est fini
     stopSerialConnexion(fd);
     return 1;
 }
 
+int hardReset(char* tty){
+    int fd = initSerialConnexion(tty);
+    if(fd<0)return 0;
+    if(!isNasAvailable(fd)) return 0;
+
+    if(sendHardreset(fd))return 0;
+    sendReboot(fd);
+    //ecrire etat nas api
+    stopSerialConnexion(fd);
+
+    //connexion(tty);
+    return 1;
+}
+
+//fonction d'initialisation qui (sur tous les ttyUSB) se connecte puis va chercher les @MAC (call api endpoint pour stockage)
+//connexion("tty");
+//getNasId(fd)
+
+//demandé au lancement de l'api
+//une biblio avec les fonctions de base + des programmes pour les fonctions haut niveau (appel séparé) <= a voir si nécessaire/utile
+
 int main(void){
 
     int test1 = connexion("/dev/ttyUSB0");
-    int test2 = available("/dev/ttyUSB0");
-    int test3 = reboot("/dev/ttyUSB0");
-    int test4 = available("/dev/ttyUSB0");
-    printf("resultat de la connexion : %d, nas dispo : %d et reboot : %d et dispo : %d\n",test1, test2, test3, test4);
-    
-    /*
-    char macAddr[15] = {0};
-    char nasType[50] = {0};
-    int available = 0;
-    int isrebooting = 0;
-    
-    int fd = initSerialConnexion("/dev/ttyUSB0");
-    if(fd<0)return printf("error\n");
-    
-    connexionNAS(fd);
-
-    available = isNasAvailable(fd);
-    printf("Dispo pour commande ? %d\n",available);
-
-    if(available){
-        getNasId(fd,macAddr);
-        getNasType(fd,nasType);
-        //isrebooting = sendReboot(fd);
-    }
-
-
-    printf("Affichage des résultats :\n");
-    printf("Type de NAS : %s\n",nasType);
-    printf("Addresse mac/identifiant unique : %s\n",macAddr);
-    printf("reboot ? %d\n",isrebooting);
-
-    stopSerialConnexion(fd);*/
-
+    //int test2 = reboot("/dev/ttyUSB0");
+    int test2 = 0;
+    printf("connexion ? : %d \n reboot ? : %d\n", test1,test2);
     return 0;
 }

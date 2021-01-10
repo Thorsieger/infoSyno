3aaed0925f4eaeae7aebd07e26c2494d85cde7f5
Thorsieger <mertz.thomas@live.fr>
2021-01-10 15:01:46 +0100

    suppression log de plus de 1 semaine

---
+++ API/clearLogs.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient, errors
+import datetime
+
+client = MongoClient("mongodb://127.0.0.1:27018", serverSelectionTimeoutMS=5000)
+db = client.mymongodb
+
+datelimite = datetime.datetime.now() - datetime.timedelta(days=7)#date de la semaine dernière
+db.logs.delete_many({'date':{"$lt":'{datelimite}'}})#suppression des logs plus vieux que cette date
